6807cee0564a7a0224bae057ee4b47178a362c32
Roman Koss (Founder) <win.maklin@gmail.com>
2020-10-19 06:53:43 +0300

    feat: auth ready

--- functions/add-movie.js
+++ functions/add-movie.js
@@ -2,11 +2,22 @@
 
 const { query } = require("./util/hasura")
 
-exports.handler = async (event) => {
+exports.handler = async (event, context) => {
+    const { user } = context.clientContext
+    const isLoggedIn = user && user.app_metadata;
+    const roles = user.app_metadata.roles || []
     const variables = JSON.parse(event.body)
-    const result = query({
+
+    if (!isLoggedIn || !roles.includes('admin')) {
+        return {
+            statusCode: 401,
+            body: 'Unauthorized',
+        }
+    }
+
+    const result = await query({
         query: `
-            mutation MyMutation($poster: String!, $id: String!, $tagline: String!, $title: String!) {
+            mutation ($poster: String!, $id: String!, $tagline: String!, $title: String!) {
                 insert_movies_one(object: {poster: $poster, id: $id, tagline: $tagline, title: $title}) {
                     id
                     title
